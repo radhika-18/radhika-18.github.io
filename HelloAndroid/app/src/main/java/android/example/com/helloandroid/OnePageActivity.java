6d911745de656bca97d3a0ef22a9bcb563b40a7d
Radhika <nairradhika18@gmail.com>
2018-04-11 23:18:09 +0530

    commit for first androidapp (UDACITY)

---
+++ HelloAndroid/app/src/main/java/android/example/com/helloandroid/OnePageActivity.java
@@ -0,0 +1,13 @@
+package android.example.com.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OnePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_page);
+    }
+}
---
+++ HelloAndroid/app/src/main/java/android/example/com/helloandroid/StartScreen.java
@@ -0,0 +1,35 @@
+package android.example.com.helloandroid;
+
+import android.app.Activity;
+import android.os.Handler;
+import android.os.Bundle;
+import android.content.Intent;
+
+/**
+ * Created by Radhika on 11-04-2018.
+ */
+
+public class StartScreen extends Activity{
+
+    private static int SPLASH_TIME_OUT = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_splash);
+
+        new Handler().postDelayed(new Runnable(){
+
+            @Override
+            public void run(){
+                Intent i  = new Intent(StartScreen.this, OnePageActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
+
+
+
