a74e9190cabeca52f6f73ff82fa93ebcd30e7214
Radhika <nairradhika18@gmail.com>
2018-04-29 20:55:31 +0530

    JUST JAVA 04.29.18

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.android.justjava;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,8 +43,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void incrementQuantity(View view) {
-        if(quantity != 100)
+        if(quantity != 100){
             this.quantity += 1;
+            ((TextView)findViewById(R.id.quantityViewTextView)).setText(this.quantity);
+        }
+
         else{
             context = getApplicationContext();
             Toast toast = Toast.makeText(context, "Only 100 coffees can be places at a time", Toast.LENGTH_LONG);
@@ -50,8 +59,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void decrementQuantity(View view) {
-        if(quantity != 0)
+        if(quantity != 0) {
             this.quantity -= 1;
+            ((TextView)findViewById(R.id.quantityViewTextView)).setText(this.quantity);
+        }
         else{
 
             context = getApplicationContext();
@@ -63,7 +74,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendInvoice(View view) {
-
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        String emailId = ((EditText)findViewById(R.id.inputnameEditText)).getText().toString()+"@gmail.com";
+        intent.putExtra(Intent.EXTRA_EMAIL, emailId);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        LocalDate localDate = LocalDate.now();
+        String subject = "Invoice for your coffee at Cafe N Crisp on" + (dtf.format(localDate));
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        String text = "<table><tr><td>Name :</td><td>" + R.id.inputnameEditText + "</td></tr><tr><td>Choice :</td><td>Whipped Cream : "
+                +(findViewById(R.id.checkBox_whippedCream)).isSelected() + "<br>Chocolate: " +(findViewById(R.id.checkBox_chocolate)).isSelected() +"</td>"
+                +"</tr><tr><td>Quantity :</td><td>" +this.quantity +"</td></tr><tr><td>Total</td><td>" + (R.string.Rs)+ this.price
+                +"</td></tr><tr><td><b></b>Tanks fr Ordering . Do visit again</b></td></tr></table>";
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     private void CalculateCost(boolean whippingCreamPresent, boolean chocolatePresent){
@@ -82,8 +108,7 @@ public class MainActivity extends AppCompatActivity {
         ((TextView)findViewById(R.id.quantityTextView)).setText(this.quantity);
         String choiceText = "Whipped Cream: " + (String.valueOf(whippingCreamCheckBox)) +"\n Chocolate: " + (String.valueOf(chocolateCheckBox));
         ((TextView)findViewById(R.id.choiceTextView)).setText(choiceText);
-        ((TextView)findViewById(R.id.totalTextView)).setText(String.valueOf(this.price));
-
+        ((TextView)findViewById(R.id.totalTextView)).setText(R.string.Rs+". "+String.valueOf(this.price));
     }
 
 }
